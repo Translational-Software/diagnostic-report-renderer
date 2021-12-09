9076e35983b56bfaf867807f03ebfda663fdac80
Madison Stanford <madison.stanford@gmail.com>
2021-12-08 17:10:04 -0800

    Fix port to use variable

--- server/index.js
+++ server/index.js
@@ -4,6 +4,6 @@ const port = process.env.PORT || 5000;;
 
 app.use(express.static('client/dist'));
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
